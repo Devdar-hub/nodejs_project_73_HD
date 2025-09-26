1dc9d7d4c42d635a140c3b43b27e9e953ef35255
Devdar-hub <darshanbhatt1799@gmail.com>
2025-09-26 23:25:38 +1000

    added new jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,26 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+// /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+// }
+
+
+// jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-}
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "reports/junit",
+        outputName: "results.xml"
+      }
+    ]
+  ]
+};
+
+export default config;
